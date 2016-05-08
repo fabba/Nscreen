475e4e2e0c9c655f2b03d3b9331e74c7a3872d15
fabba <fabian@crowdtruth.org>
2016-05-08 15:22:28 -0400

    merge

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
     define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_DATABASE', 'nscreen_members');
+    define('DB_USER', 'nscreen');
+    define('DB_PASSWORD', 'nscreen');
+    define('DB_DATABASE', 'nscreen_db');
 ?>
